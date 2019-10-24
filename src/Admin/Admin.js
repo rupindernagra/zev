b6afbae04b2b4c8276cd336eadc20fbd8235d865
kartique <kartique79@gmail.com>
2019-10-24 14:41:19 +0530

    changes made to create pages

---
+++ src/Admin/Admin.js
@@ -0,0 +1,122 @@
+import React, { Component } from 'react';
+//import { Row, Col, Inputs, Button } from 'adminlte-2-react'; // can use in other pages for layout
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Redirect } from 'react-router-dom';
+import {Container,Row,Col,Form,Button} from 'react-bootstrap';
+import './admin-login.css';
+const validEmailRegex = /^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/;
+
+const countErrors = (errors) => {
+  let count = 0;
+  Object.values(errors).forEach(
+    (val) => val.length > 0 && (count = count + 1)
+  );
+  return count;
+}
+
+const validateForm = (errors) => {
+  let valid = true;
+  Object.values(errors).forEach(
+    (val) => val.length > 0 && (valid = false)
+  );
+  return valid;
+}
+
+
+export default class Admin extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      formValid: false,
+      errorCount: null,
+      validateUser: null,
+      errors: {
+        email: '',
+        password: '',
+      }
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange = (event) => {
+    event.preventDefault();
+    const { name, value } = event.target;
+    let errors = this.state.errors;   
+    switch (name) {
+      case 'email':
+        errors.email =
+          validEmailRegex.test(value)
+            ? ''
+            : 'Email is not valid!';
+        break;
+      case 'password':
+        errors.password =
+          value.length < 8
+            ? 'Password must be 8 characters long!' : '';
+        break;
+      default:
+        break;
+    }
+
+    this.setState({ errors, [name]: value });
+    this.setState({formValid: validateForm(this.state.errors)});
+    this.setState({errorCount: countErrors(this.state.errors)});
+  }
+
+  render() {
+    if(localStorage.getItem('login')){
+      return <Redirect to='/dashboard' />
+    }
+    const { errors } = this.state;
+    return (
+      <Container>
+        <Row>
+          <Col md={{span:4, offset:4}} xs={{span:4, offset:4}}>
+          <Form className="admin-login">
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control name="email" type="email" placeholder="Enter email"  onChange={this.handleChange} noValidate />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+              {errors.email.length > 0 &&
+                    <span className='error pr-15 text-right text-danger d-block'>{errors.email}</span>}
+            </Form.Group>
+
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control name="password" type="password" placeholder="Password" onChange={this.handleChange} noValidate />
+              {errors.password.length > 0 &&
+                    <span className='error pr-15 text-right text-danger d-block'>{errors.password}</span>}
+            </Form.Group>
+            <div className="text-right">
+              <Button variant="primary" type="submit" onClick={this.handleSubmit}>
+                Login
+              </Button>
+              {/* {this.state.errorCount !== null ? <p className="form-status text-center">Form is {formValid ? 'valid ✅' : 'invalid ❌'}</p> : ''} */}
+              {this.state.validateUser !== null ? <p className="form-status alert alert-danger text-center">Invalid User Credentials</p> : ''}
+            </div>
+          </Form>
+          </Col>
+        </Row>
+      </Container>
+    )
+  }
+    
+  handleSubmit(event) {
+    event.preventDefault();
+    if(this.state.formValid===false){
+      return false;
+    }
+    if(this.state.email==='admin@admin.com' && this.state.password==='admin123'){
+      this.setState({validateUser: null});
+      console.log(this.state);
+      localStorage.setItem('login',true);
+    }else{
+      console.log("check");
+      this.setState({validateUser: true});
+    }
+  }
+
+}
--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Registration from './Registration/Registration';
+import Admin from './Admin/Admin';
+import Dashboard from './Dashboard/Dashboard';
+import Profile from './Dashboard/Dashboard';
 
 class App extends Component{
     render() {
-      return (        
-          <Registration />
-      )
+      return  <Router>
+                <Switch>
+                  <Route exact path="/" component={Registration}/>
+                  <Route path="/admin" component={Admin}/>
+                  <Route path="/dashboard" component={Dashboard}/>
+                  <Route path="/profile" component={Profile}/>
+                </Switch>
+              </Router>
     }
-  
 }
 
 export default App;
