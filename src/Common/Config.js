a6e130a10811ab9e637417f97892626748e0cc61
sameerpujani <sameerpujanigndpc@gmail.com>
2019-12-05 16:26:14 +0530

    update config and gitignore

--- src/Common/Config.js
+++ src/Common/Config.js
@@ -1,5 +1,5 @@
 export const Config = {
     server : {
-        url: (process.env.NODE_ENV === 'production') ? "https://myserver.com" : "http://localhost:3001"
+        url: "http://localhost:3001"
     }
 };
