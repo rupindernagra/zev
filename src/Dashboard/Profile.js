045d6450479f3a66d93d127e12f29a0aca88c19c
sameerpujani <sameerpujanigndpc@gmail.com>
2019-12-10 10:47:57 +0530

    Avatar preview before upload

--- src/Dashboard/Profile.js
+++ src/Dashboard/Profile.js
@@ -2,11 +2,16 @@ import React, { Component } from 'react';
 import API from '../Common/API';
 import './dashboard.css';
 import { Content, Row, Col, Box } from 'adminlte-2-react';
+const JSAlert = require('js-alert');
 
 export default class Profile extends Component {
     constructor(props) {
         super(props);
-        this.state = { user: {} }
+        this.state = {
+            user: {},
+            file: null,
+            saveButtonVisible: false
+        }
         this.api = new API();
     }
 
@@ -23,7 +28,9 @@ export default class Profile extends Component {
     fileSelectedHandler = (event) => {
         this.setState({
             selectedFile: event.target.files[0],
-            avatarUpdated: false
+            avatarUpdated: false,
+            file: URL.createObjectURL(event.target.files[0]),
+            saveButtonVisible: true
         });
     }
 
@@ -39,6 +46,10 @@ export default class Profile extends Component {
             return res.json()
         }).then(data => {
             console.log('up', data);
+            this.setState({
+                saveButtonVisible: false
+            });
+            JSAlert.alert("Avatar uploaded!", "", JSAlert.Icons.Success).dismissIn(2000);
         }).catch(err => {
             console.log('Upload ERR: ', err);
         })
@@ -52,8 +63,8 @@ export default class Profile extends Component {
                     <Box title="Profile" type="primary">
                         <Col xs={{ span: 8, offset: 2 }} className="text-center">
                             <div className="userDP" onClick={() => this.fileInput.click()}>
-                                {user.image ? (
-                                    <img className="avatar" alt="avatar" src={this.api.avatarUrl + user.image} />
+                                {this.state.file || user.image ? (
+                                    <img className="avatar" alt="avatar" src={this.state.file || (this.api.avatarUrl + user.image)} />
                                 ) : (
                                         <i className="fa fa-user user"></i>
                                     )}
@@ -64,8 +75,14 @@ export default class Profile extends Component {
                                 onChange={this.fileSelectedHandler}
                                 ref={fileInput => this.fileInput = fileInput} />
 
-                            <button onClick={this.fileSubmitHandler}>Upload</button>
-                            <div className="userName">
+                            {this.state.saveButtonVisible ? (
+                                // <button onClick={this.fileSubmitHandler}>Save Image</button>
+                                <button onClick={this.fileSubmitHandler} class="ui labeled icon primary big button">
+                                    <i class="icon save"></i>
+                                    Save Avatar
+                                </button>
+                            ) : null}
+                            <div className="userName mt-3">
                                 {/* <input type="text" name="username" value="Kevin Parker" readOnly="true" />  */}
                                 {user.firstname}
                                 {user.lastname ? ` ${user.lastname}` : ''}
